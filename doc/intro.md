ddef758cfdfaebf718e91bedd267610e5e45ee1e
Shriphani Palakodety <shriphanip@gmail.com>
2015-08-29 18:35:36 -0700

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clj-crfsuite
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clj_crfsuite/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-crfsuite.core-test
+  (:require [clojure.test :refer :all]
+            [clj-crfsuite.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
