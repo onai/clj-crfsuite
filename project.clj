8f9e3ee5cc9cee2e3ca9df2f966027fccad9c2b0
Shriphani Palakodety <shriphanip@gmail.com>
2015-09-01 23:24:58 -0700

    more shit

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
-(defproject clj-crfsuite "0.1.0-SNAPSHOT"
+(defproject clj-crfsuite "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.github.vinhkhuc/jcrfsuite "0.6"]])
+                 [com.github.vinhkhuc/jcrfsuite "0.6"]]
+  :repositories {"kurultai" {:url "s3p://kurultai/releases/" :username :env :passphrase :env}}
+  :plugins [[s3-wagon-private "1.1.2"]])
