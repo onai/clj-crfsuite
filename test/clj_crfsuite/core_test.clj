8415f5acd8cc4139a6adeacc6dde4c3357dbc680
Shriphani Palakodety <shriphanip@gmail.com>
2016-02-03 15:02:24 -0800

    testing the newer types we added

--- test/clj_crfsuite/core_test.clj
+++ test/clj_crfsuite/core_test.clj
@@ -36,3 +36,36 @@
           (tag [{:feat1 2.0, :feat2 3.0}] "trainmodel.crfsuite")))
 
       (catch Exception e (is false)))))
+
+(deftest types-test
+  (testing "Are the various supported types conveniently?"
+    (try
+      (is
+       (nil?
+        (train [
+                [{:feat1 "2.0", :feat2 3.0} ; string and double vals
+                 {:feat1 3.0, :feat2 4.0}
+                 ]
+            
+                [{:feat1 2.0, :feat2 3.0}   ; all double
+                 {:feat1 4.0, :feat2 5.0}
+                 ]
+                
+
+                [
+                 {:feat1 true, :feat2 false} ; all boolean
+                 {:feat1 false, :feat2 true}
+                 ]]
+
+               [
+                ["y1", "y2"],
+                ["y1", "y3"]
+                ["y4", "y5"]]
+               "trainmodel.crfsuite")))
+
+      (is
+       (not
+        (nil?
+         (tag [{:feat1 true, :feat2 true}] "trainmodel.crfsuite"))))
+
+      (catch Exception e (is false)))))
