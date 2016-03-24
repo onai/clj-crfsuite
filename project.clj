ff640533a09b40af22f04db2f68b7e916b046d4b
Shriphani Palakodety <shriphanip@gmail.com>
2016-03-24 14:56:10 -0700

    version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-crfsuite "0.4.0"
+(defproject clj-crfsuite "0.5.0"
   :description "Clojure wrapper for CRFSuite"
   :url "https://github.com/onutech/clj-crfsuite"
   :license {:name "Apache V2 License"
